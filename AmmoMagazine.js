0e8b8fb5a61b968491b0c3e0d3dd21e974c93806
LiteSoul <exorcismo@gmail.com>
2025-03-09 01:27:14 -0300

    ammo is sprite

--- AmmoMagazine.js
+++ AmmoMagazine.js
@@ -10,9 +10,12 @@ export class AmmoMagazine {
   }
 
   createModel() {
-    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const material = new THREE.MeshPhongMaterial({ color: 0x0000ff }); // Blue color
-    const magazine = new THREE.Mesh(geometry, material);
-    return magazine;
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load("images/ammo.png");
+    const material = new THREE.SpriteMaterial({ map: texture });
+    const sprite = new THREE.Sprite(material);
+    sprite.scale.set(0.32, 0.32, 1); // Assuming 32x32 pixels
+    sprite.position.y += 0.16; // Adjust to place bottom on ground
+    return sprite;
   }
 }
